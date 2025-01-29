955ca6c300c3630a44a0d98a64f85b58df78c590
Teletrup <saletrapotasowatosyf@gmail.com>
2025-01-29 07:45:28 +0100

    !

---
+++ src/app.js
@@ -0,0 +1,75 @@
+document.body.innerHTML = `
+<pre>
+okay
+so this is my blog or something
+I'll write about whatever I find interesting to write about
+Not even (you can ask Steven)
+I'll write about anything
+it's 2:43am
+I'll write about space gardens
+  those are gardens in space
+  rotating
+  how plants grow in them
+  how small they can be
+    gardens, that is
+  I thought about writing a simulation
+    some plants modeled as L-systems
+    subject to coriolis forces
+I'll write about ideas for rich people
+  for people who have too much money and want to lose it
+  in an interesting way
+...
+What kind of vibe am I aiming for?
+  Turluscent?
+    Buckminster Fuller had this word "dymaxion"
+      he was like dymaxion this, dymaxion that
+      dymaxion car, dymaxion house, dymaxion map, dymaxion sleep
+      something something "dynamic maximization"
+        I believe he made it up after he came up with the word
+        it just sounded cool
+    Well I have my word too
+    that word is "turluscent"
+    what's turluscent?
+    I don't know for sure
+    I walk around and when I see something that looks turluscent
+    ...
+    turbulent & translucent
+    opposite of turluscent would have to be
+      laminar & opaque
+    [tensegrity]
+      cat trees
+      arithmetic
+so right now it's just a bunch of text in &lt;pre&gt; tags
+I don't know how to do
+the idea is, I'll put out a lot of unfinished stuff out there
+And I'll come around and make it even less unfinished.
+I'll put out unfinished stuff out there on the internet.
+To create tension!
+Tension on the internet 
+  Yo, when I was a little kid... I had this dream
+  There were two massive metal bodies moving apart
+  and they were stretching this tiny fishing line.
+  And it was accompanied by 
+I'll experiment with the new forms of publishing written work
+that can't work on paper
+with internet you can
+and this is really cool
+[whole life to write first book]
+[blah blah to write second]
+...
+Yo,
+This site supports JavaScript
+It supports it when you least expect it
+JavaScript is my favorite esoteric programming language
+I don't like how bad choices pile up on it
+[designing systems that will have stuff built on top]
+...
+on structural text editors
+  macros
+the deal with lisp
+The methodology of organization
+  Premature organization leads to spurious abstraction
+    rly?
+differential pulley
+</pre>
+`;
